0baaeac0934037e4dd9ad0b4d37aba1d16fa3f8c
Gabriel Ramos Rios <gabriel.ramos.rios@hotmail.com>
2022-07-13 21:44:34 -0300

    add directives component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'diretivas';
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = 'Gabriel';
+  text = '';
+
+  pessoas = [
+    {
+      nome: "Ivonaldo",
+      sobrenome:"Soares"
+    },
+    {
+      nome: "Maria",
+      sobrenome:"Silva"
+    },
+    {
+      nome: "Joao",
+      sobrenome:"Pereira"
+    },
+    {
+      nome: "Marcio",
+      sobrenome:"Santos"
+    },    
+  ]
+
+  constructor (){
+
+  }
+
+  ngOnInit() {
+    let interval = setInterval (( )=>{
+      this.count++;
+      if (this.count ===10){
+        clearInterval (interval);
+      }
+    }, 1000)
+  }
+  clicou(nome:string): void {
+    console.log('clicou em mim', nome)
+  }
 }
